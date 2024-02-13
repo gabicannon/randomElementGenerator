a11f111604019be55d79bd72466c8115ef97c1a8
Gabriella Cannon <gabicannon11@gmail.com>
2024-02-13 08:56:19 -0500

    First commit

---
+++ element.js
@@ -0,0 +1,5 @@
+const element = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", 
+"Carbon", "Nitrogen", "Oxygen", "Fluorine", "Neon", 
+"Sodium", "Magnesium", "Aluminum", "Silicon", "Phosphorus", 
+"Sulfur", "Chlorine", "Argon", "Potassium", "Calcium", 
+"Scandium", "Titanium", "Vanadium", "Chromium", "Manganese"]
---
+++ funFact.js
@@ -0,0 +1,7 @@
+//remove from file after funFact array has been created
+const newElement = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", 
+"Carbon", "Nitrogen", "Oxygen", "Fluorine", "Neon", 
+"Sodium", "Magnesium", "Aluminum", "Silicon", "Phosphorus", 
+"Sulfur", "Chlorine", "Argon", "Potassium", "Calcium", 
+"Scandium", "Titanium", "Vanadium", "Chromium", "Manganese"]
+
