79eaa0ddd7794ef42e0b40fa15730f5af7a23861
Gabriella Cannon <gabicannon11@gmail.com>
2024-02-13 09:59:23 -0500

    Added Random Number Generator

---
+++ Main.js
@@ -0,0 +1,4 @@
+import element from "./element";
+import funFact from "./funFact";
+
+const randomElement = `You are ${element}. Here is a fun fact about that element, ${funFact}`
--- element.js
+++ element.js
@@ -3,3 +3,8 @@ const element = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron",
 "Sodium", "Magnesium", "Aluminum", "Silicon", "Phosphorus", 
 "Sulfur", "Chlorine", "Argon", "Potassium", "Calcium", 
 "Scandium", "Titanium", "Vanadium", "Chromium", "Manganese"]
+
+//generate a random number to pull from the element array
+const randomNumberGenerator = Math.floor(Math.random()* 25);
+
+export default element;
